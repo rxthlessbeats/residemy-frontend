879e699c60a72ad9b91a7e3b70800c9fed9e91e0
rxthlessbeats <lutinyu@gmail.com>
2023-11-29 13:28:59 +0800

    demo v2

---
+++ src/pages/auth/.ipynb_checkpoints/login-checkpoint.js
@@ -0,0 +1,213 @@
+import { useCallback, useState } from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import {
+  Alert,
+  Box,
+  Button,
+  FormHelperText,
+  Link,
+  Stack,
+  Tab,
+  Tabs,
+  TextField,
+  Typography
+} from '@mui/material';
+import { useAuth } from 'src/hooks/use-auth';
+import { Layout as AuthLayout } from 'src/layouts/auth/layout';
+
+const Page = () => {
+  const router = useRouter();
+  const auth = useAuth();
+  const [method, setMethod] = useState('email');
+
+  const handleLogin = () => {
+    const state = 'login'; 
+    const client_id = process.env.NEXT_PUBLIC_LINE_CLIENT_ID;
+    const redirect_uri = `${process.env.NEXT_PUBLIC_BASE_URL}/api/line-callback`;
+    const lineAuthUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&state=${state}&scope=openid%20profile`;
+    router.push(lineAuthUrl);
+  };
+
+  const handleMethodChange = useCallback(
+    (event, value) => {
+      setMethod(value);
+    },
+    []
+  );
+
+  const handleSkip = useCallback(
+    () => {
+      auth.skip();
+      router.push('/');
+    },
+    [auth, router]
+  );
+
+  return (
+    <>
+      <Head>
+        <title>
+          登入 | Residemy
+        </title>
+      </Head>
+      <Box
+        sx={{
+          backgroundColor: 'background.paper',
+          flex: '1 1 auto',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 550,
+            px: 3,
+            py: '100px',
+            width: '100%'
+          }}
+        >
+          <div>
+            <Stack
+              spacing={1}
+              sx={{ mb: 3 }}
+            >
+              <Typography variant="h4">
+                Login
+              </Typography>
+              {/* <Typography
+                color="text.secondary"
+                variant="body2"
+              >
+                Don&apos;t have an account?
+                &nbsp;
+                <Link
+                  component={NextLink}
+                  href="/auth/register"
+                  underline="hover"
+                  variant="subtitle2"
+                >
+                  Register
+                </Link>
+              </Typography> */}
+            </Stack>
+            <Tabs
+              onChange={handleMethodChange}
+              sx={{ mb: 3 }}
+              value={method}
+            >
+              <Tab
+                label="LINE"
+                value="email"
+              />
+              {/* <Tab
+                label="Phone Number"
+                value="phoneNumber"
+              /> */}
+            </Tabs>
+            {method === 'email' && (
+              <form
+                // noValidate
+                // onSubmit={formik.handleSubmit}
+              >
+                {/* <Stack spacing={3}>
+                  <TextField
+                    error={!!(formik.touched.email && formik.errors.email)}
+                    fullWidth
+                    helperText={formik.touched.email && formik.errors.email}
+                    label="Email Address"
+                    name="email"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="email"
+                    value={formik.values.email}
+                  />
+                  <TextField
+                    error={!!(formik.touched.password && formik.errors.password)}
+                    fullWidth
+                    helperText={formik.touched.password && formik.errors.password}
+                    label="Password"
+                    name="password"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="password"
+                    value={formik.values.password}
+                  />
+                </Stack>
+                <FormHelperText sx={{ mt: 1 }}>
+                  Optionally you can skip.
+                </FormHelperText>
+                {formik.errors.submit && (
+                  <Typography
+                    color="error"
+                    sx={{ mt: 3 }}
+                    variant="body2"
+                  >
+                    {formik.errors.submit}
+                  </Typography>
+                )}
+                <Button
+                  fullWidth
+                  size="large"
+                  sx={{ mt: 3 }}
+                  type="submit"
+                  variant="contained"
+                >
+                  Continue
+                </Button> */}
+                <Button
+                  fullWidth
+                  size="large"
+                  sx={{ mt: 3 }}
+                  onClick={handleLogin}
+                  variant="contained"
+                >
+                  LOGIN WITH LINE
+                </Button>
+                {/* <Button
+                  fullWidth
+                  size="large"
+                  sx={{ mt: 3 }}
+                  onClick={handleSkip}
+                >
+                  Skip authentication
+                </Button> */}
+                {/* <Alert
+                  color="primary"
+                  severity="info"
+                  sx={{ mt: 3 }}
+                >
+                  <div>
+                    You can use <b>demo@devias.io</b> and password <b>Password123!</b>
+                  </div>
+                </Alert> */}
+              </form>
+            )}
+            {method === 'phoneNumber' && (
+              <div>
+                <Typography
+                  sx={{ mb: 1 }}
+                  variant="h6"
+                >
+                  Not available in the demo
+                </Typography>
+                <Typography color="text.secondary">
+                  To prevent unnecessary costs we disabled this feature in the demo.
+                </Typography>
+              </div>
+            )}
+          </div>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => (
+  <AuthLayout>
+    {page}
+  </AuthLayout>
+);
+
+export default Page;
